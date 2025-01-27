f7f9077325a651bd2033896ff92614629d4053ac
BenSTV <stevielyfiles@gmail.com>
2025-01-27 15:05:45 +0700

    finish logo

--- app/_components/productContainer.jsx
+++ app/_components/productContainer.jsx
@@ -1,11 +1,23 @@
 "use client";
 import Link from "next/link";
 
-export default function ProductContainer({ link, productType }) {
+export default function ProductContainer({ link, productType, logo }) {
   return (
-    <Link href={link}>
-      <div className="bg-white h-[170px] w-[170px] flex justify-center items-center">
-        <h1 className="text-black">{productType}</h1>
+    <Link href={link} className="flex justify-center">
+      <div className="relative bg-white h-[150px] w-[150px] flex items-center justify-center group overflow-hidden">
+        {/* โลโก้ */}
+        <img
+          src={logo}
+          alt={`${productType} Logo`}
+          width={70}
+          className="relative z-10 transition-opacity duration-300 ease-in-out"
+        />
+        {/* พื้นหลังจาง */}
+        <div className="hidden group-hover:block bg-white opacity-80 w-full h-full absolute z-0 transition-opacity duration-300"></div>
+        {/* ชื่อผลิตภัณฑ์ */}
+        <h1 className="hidden group-hover:block text-black absolute z-20 opacity-0 group-hover:opacity-100 scale-95 group-hover:scale-100 transition-all duration-300 ease-in-out text-center">
+          {productType}
+        </h1>
       </div>
     </Link>
   );
--- app/products/page.jsx
+++ app/products/page.jsx
@@ -1,26 +1,26 @@
 import ProductContainer from "../_components/productContainer";
+
 export default function Page() {
   const container = [
-    { id: 1, productType: "TOYOTA", link: "/toyota" },
-    { id: 2, productType: "HONDA", link: "/honda" },
-    { id: 3, productType: "NISSAN", link: "/nisan" },
-    { id: 4, productType: "SUZUKI", link: "/suzuki" },
-    { id: 5, productType: "BYD", link: "/byd" },
-    { id: 6, productType: "TESLA", link: "/tesla" },
-    { id: 7, productType: "MAZDA", link: "/mazda" },
+    { id: 1, productType: "TOYOTA", link: "/toyota", logo: "toyota.png" },
+    { id: 2, productType: "HONDA", link: "/honda", logo: "honda.png" },
+    { id: 3, productType: "NISSAN", link: "/nissan", logo: "nissan.png" },
+    { id: 4, productType: "SUZUKI", link: "/suzuki", logo: "suzuki.png" },
+    { id: 5, productType: "BYD", link: "/byd", logo: "byd.png" },
+    { id: 6, productType: "TESLA", link: "/tesla", logo: "tesla.png" },
+    { id: 7, productType: "MAZDA", link: "/mazda", logo: "mazda.png" },
   ];
+
   return (
     <div className="grid grid-cols-2 gap-2 mt-6">
-      {container.map((el, index) => {
-        console.log(el);
-        return (
-          <ProductContainer
-            key={el.id}
-            productType={el.productType}
-            link={el.link}
-          />
-        );
-      })}
+      {container.map((el) => (
+        <ProductContainer
+          key={el.id}
+          productType={el.productType}
+          link={el.link}
+          logo={el.logo}
+        />
+      ))}
     </div>
   );
 }
