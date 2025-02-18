ab9d49af56a9cf25546f8e0049811ce4eb70e1f9
unknown <stevielyfiles@gmail.com>
2025-02-18 20:49:13 +0700

    5th commit

--- app/_components/hero2.jsx
+++ app/_components/hero2.jsx
@@ -5,21 +5,21 @@ export default function Hero2() {
   return (
     <div className="w-full  flex justify-between mt-4 lg:my-hidden relative border-[2px] border-[#0000fe] rounded-[16px]">
       <div className=" bg-black text-white  ">
-        <div className=" absolute z-40  p-4 ">
-          <span className="text-[12px] lg:text-[30px] ">abcdefgh</span>
-          <h1 className="font-bold text-[24px] lg:text-[58px] ">ABCDEFGH</h1>
-          <p className="text-[14px] lg:text-[25px] ">
+        <div className=" absolute z-40  p-2  ">
+          <span className="text-[12px] sm:text-[18px] lg:text-[30px] ">abcdefgh</span>
+          <h1 className="font-bold text-[24px] sm:text-[38px] lg:text-[58px] ">ABCDEFGH</h1>
+          <p className="text-[10px] sm:text-[] lg:text-[25px] ">
             ABCDEFGHIJ MKLNOPQRSTUVWX YZ
           </p>
-          <div className=" lg:mt-[50px] lg:gap-8 hidden lg:flex justify-start gap-2">
-            <button className="text-[18px] border border-[#0000fe] p-1 lg:p-4 w-[180px] rounded-[16px] hover:bg-white hover:text-[#0000fe] font-bold hover:border-[#0000fe] transition-all duration-500 ease-in-out">
+          <div className=" lg:mt-[50px] lg:gap-8 hidden sm:flex sm:mt-4 lg:flex justify-start gap-2">
+            <button className="lg:text-[18px] sm:text-[14px] border border-[#0000fe] p-1   lg:p-4 sm:w-[120px] lg:w-[180px] rounded-[16px] 
+    bg-gradient-to-b from-[#0000fe] to-[#000939] 
+    hover:from-[#0000d1] hover:to-[#000626] 
+    transition-all duration-500 ease-in-out">
               ดูสินค้าทั้งหมด
             </button>
             <button
-              className="text-[18px] border border-white p-1 lg:p-4 w-[180px] rounded-[16px] 
-    bg-gradient-to-b from-[#0000fe] to-[#000939] 
-    hover:from-[#0000d1] hover:to-[#000626] 
-    transition-all duration-500 ease-in-out"
+              className="lg:text-[18px] sm:text-[14px] border border-[#0000fe] p-1   lg:p-4 sm:w-[120px] lg:w-[180px] rounded-[16px] hover:bg-white hover:text-[#0000fe] font-bold hover:border-[#0000fe] transition-all duration-500 ease-in-out"
             >
               ติดต่อเรา
             </button>
