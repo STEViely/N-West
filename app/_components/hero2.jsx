68039d04485efff1525a0d6b46e0d611335cdea0
BenSTV <stevielyfiles@gmail.com>
2025-02-18 19:37:07 +0700

    3rd testing

--- app/_components/hero2.jsx
+++ app/_components/hero2.jsx
@@ -3,31 +3,35 @@ import Image from "next/image";
 
 export default function Hero2() {
   return (
-    <div className="w-full  flex justify-center mt-4 relative border-[2px] border-[#0000fe]">
-      {/* <div className="w-[50%] bg-black text-white lg:p-20 "> */}
-      {/* <span className="text-[30px]">abcdefgh</span> */}
-      {/* <h1 className="font-bold text-[58px]">ABCDEFGH</h1>
-        <p className="text-[25px]">ABCDEFGHIJ MKLNOPQRSTUVWX YZ</p>
-        <div className="mt-[100px] flex justify-around items-center">
-          <button className="border border-white p-4 w-[180px] rounded-[16px] hover:bg-white hover:text-[#0000fe] text-bold hover:border-[#0000fe]  transition-all duration-500 ease-in-out">
-            ดูสินค้าทั้งหมด
-          </button>
-          <button
-            className="border border-white p-4 w-[180px] rounded-[16px] 
+    <div className="w-full  flex justify-between mt-4 my-8 lg:my-hidden relative border-[2px] border-[#0000fe]">
+      <div className=" bg-black text-white lg:p-20  ">
+        <div className=" absolute z-50 p-4 ">
+          <span className="text-[12px] lg:text-[30px] ">abcdefgh</span>
+          <h1 className="font-bold text-[24px] lg:text-[58px] ">ABCDEFGH</h1>
+          <p className="text-[14px] lg:text-[25px] ">
+            ABCDEFGHIJ MKLNOPQRSTUVWX YZ
+          </p>
+          <div className="  mt-[30px] lg:mt-[100px] lg:flex justify-start gap-2">
+            <button className=" text-[14px] border border-[#0000fe] p-1 lg:p-4 w-[100px] rounded-[16px] hover:bg-white hover:text-[#0000fe] text-bold hover:border-[#0000fe]  transition-all duration-500 ease-in-out">
+              ดูสินค้าทั้งหมด
+            </button>
+            <button
+              className=" text-[14px] border border-white p-1  lg:p-4 w-[100px] rounded-[16px] 
                    bg-gradient-to-b from-[#0000fe] to-[#000939] 
                    hover:from-[#0000d1] hover:to-[#000626] 
                    transition-all duration-500 ease-in-out"
-          >
-            ติดต่อเรา
-          </button>
-        </div> */}
-      {/* </div> */}
+            >
+              ติดต่อเรา
+            </button>
+          </div>
+        </div>
+      </div>
 
-      {/* <div className="w-[50%] relative">
-        <Image src={i6} alt="Hero" className="w-[50%] h-auto" />
+      <div className="w-[50%] relative">
+        <Image src={i6} alt="Hero" className="w-full h-auto" />
 
         <div className="absolute inset-0 bg-gradient-to-r from-black via-transparent to-transparent"></div>
-      </div> */}
+      </div>
     </div>
   );
 }
--- app/_components/subNavBar.jsx
+++ app/_components/subNavBar.jsx
@@ -21,7 +21,7 @@ export default function SubNavbar({ bg }) {
   return (
     <header>
       <nav className="w-full flex justify-center ">
-        <ul className="flex flex-wrap justify-center gap-1 text-white text-[14px] lg:text-[20px] font-light lg:flex lg:justify-center justify-items-center">
+        <ul className="flex flex-wrap justify-between gap-1 text-white text-[14px] lg:text-[20px] font-light lg:flex lg:justify-center justify-items-center">
           {[
             { name: "TOYOTA", href: "/toyota" },
             { name: "HONDA", href: "/honda" },
@@ -34,7 +34,7 @@ export default function SubNavbar({ bg }) {
           ].map((item, index) => (
             <li
               key={index}
-              className={`w-[120px] lg:w-[180px] lg:h-[40px] p-0.5 bg-gradient-to-b from-[#0000fe] to-[#000939] text-center flex items-center justify-center hover:underline ${
+              className={`w-[84px] lg:w-[180px] lg:h-[80px] p-0.5 bg-gradient-to-b from-[#0000fe] to-[#000939] text-center flex items-center justify-center hover:underline ${
                 isActive(item.href) ? "underline" : ""
               }`}
             >
