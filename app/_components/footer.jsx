4c98e20e81564d49b34c4be7ff8e89dc177e1cd1
unknown <stevielyfiles@gmail.com>
2025-02-23 15:00:19 +0700

    test 2322025

--- app/_components/footer.jsx
+++ app/_components/footer.jsx
@@ -3,49 +3,30 @@ import NWEST from "@/public/NWEST.png";
 import Link from "next/link";
 export default function Footer() {
   return (
-    <div className="w-full mt-4 px-10 py-5 bg-[#1C1C1C]">
-      <div className="flex gap- justify-around">
-        <div className="w-[25%] flex flex-col gap-4">
-          <div>
-            <Image src={NWEST} alt="logo" width={60} height={20} />
-          </div>
-          <p className="text-white font-light text-[8px]">
-            intergio a beautidful digital tranformation consultrancy & solfware
-            development company that provide IT solotutions.
-          </p>
+    <div className="flex justify-around bg-[#1C1C1C] mt-8 p-4">
+      <div className="w-[30%]"><Image src={NWEST} alt="N-WEST STREET DRIVE" /></div>
+      <div className="w-[30%] flex justify-start items-end flex-col text-white ">
+        <p className="text-[12px] font-bold">Contact</p>
+        <br />
+        <p className="text-[10px] font-serif">บางนา-ตราด กม.26 เอแบคบางนา ต.บางเสาธง อ.บางเสาธง สมุทรปราการ,  10540</p>
         </div>
-        <div className=" flex flex-col gap-4">
-          <div>
-            <p className="text-white text-[8px] font-bold">Contact</p>
-          </div>
-          <p className="text-white font-light text-[8px]">
-            Adress: 2 7 Thachalong,
-            <br /> Bangna, 10002, Thailand{" "}
-          </p>
-          <p className="text-white font-light text-[8px]">
-            Email: nwestautopart@mail.com{" "}
-          </p>
-          <p className="text-white font-light text-[8px]">
-            Phone: +66 12345678
-          </p>
+      <div className="w-[40%] flex justify-start items-end flex-col text-white font-thin text-[12px]">
+        <p className="text-[12px] font-bold">Service</p>
+        <br />
+        <p className="text-[10px] ">กระจกมองข้างสีฟ้า</p>
+      
         </div>
-        <div className=" flex flex-col gap-4">
-          <div>
-            <p className="text-white text-[8px] font-bold">Contact</p>
-          </div>
-          <p className="text-white font-light text-[8px]">mocking up</p>
-          <p className="text-white font-light text-[8px]">delivery</p>
+      <div className="w-[40%] flex justify-start items-end flex-col text-white font-thin text-[12px]">
+        <p className="text-[12px] font-bold">Products</p>
+        <br />
+        <p className="text-[10px] ">TOYOTA</p>
+        <p className="text-[10px] ">HONDA</p>
+        <p className="text-[10px] ">TESLA</p>
+        <p className="text-[10px] ">NISAN</p>
+        <p className="text-[10px] ">BYD</p>
+        <p className="text-[10px] ">SUZUKI</p>
         </div>
-        <div className=" flex flex-col gap-4">
-          <div>
-            <p className="text-white text-[8px] font-bold">Contact</p>
-          </div>
-          <p className="text-white font-light text-[8px]">mirror</p>
-          <p className="text-white font-light text-[8px]">sport light</p>
-          <p className="text-white font-light text-[8px]">rare light</p>
-          <p className="text-white font-light text-[8px]">other</p>
-        </div>
-      </div>
+      
     </div>
   );
 }
